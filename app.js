02f26aced078e62ad442037bd3acc664bafdc550
Andrei Tudor <andrei.tudor27@gmail.com>
2018-11-05 15:14:12 +0100

    CORS

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
+app.use(cors())
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(logger('dev'));
