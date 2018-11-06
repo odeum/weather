33f71b8b35dcb384e6398704cd5fd0b03941fefc
Christian Broberg <cb@webhouse.dk>
2018-11-06 17:33:42 +0100

    Small changes before goint to the new SentiAPI endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 var getWeather = require('../weatherAPI/data')
 const weatherData = {
 	"2018-11-5": "Foggy",
@@ -9,8 +9,8 @@ const weatherData = {
 router.get('/:date/:lat/:long', async function (req, res, next) {
 	console.log(req.params.date, req.params.lat, req.params.long)
 	let rs = await getWeather(req.params.date, req.params.lat, req.params.long)
-	console.log(rs);
+	console.log(rs)
 	res.send(JSON.stringify(rs))
-});
+})
 
-module.exports = router;
+module.exports = router
